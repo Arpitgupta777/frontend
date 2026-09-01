8018e14f27861f5380c88264291218ef3dd37f71
Arpit Gupta <arpitmeena5852@gmail.com>
2026-09-01 19:57:40 +0530

    new changes

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,11 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   
-  server: {
-    proxy: {
-      '/api': 'https://backend-4o41.onrender.com/'
-    }
-  },
+  // server: {
+  //   proxy: {
+  //     '/api': 'https://backend-4o41.onrender.com/'
+  //   }
+  // },
 
   plugins: [react()],
 })
