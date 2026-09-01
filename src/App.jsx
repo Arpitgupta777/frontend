33df24592608072bc9fee81a62c0893535e8f8a8
Arpit Gupta <arpitmeena5852@gmail.com>
2026-09-01 21:26:56 +0530

    dist updated

--- src/App.jsx
+++ src/App.jsx
@@ -6,23 +6,23 @@ function App() {
   const [jokes,setJokes] = useState([]);
 
   useEffect(()=>{
-        axios.get(`${process.env.VITE_BACKEND_URL}/api/jokes`).then((response)=>{
+        axios.get(`${API_BASE}/api/jokes`).then((response)=>{
           setJokes(response.data); 
         }).catch((error)=>{
           console.log(error);
           
         })
-  })
+  }, [])
 
   return (
    <>
    <h1> Hi Hello</h1>
    <p> Jokes: {jokes.length}</p>  
    {
-    jokes.map((jokes,index)=>(
-      <div key= {jokes.id}>
-        <h3>{jokes.title}</h3>
-        <p>{jokes.content}</p>
+    jokes.map((joke,index)=>(
+      <div key= {joke.id}>
+        <h3>{joke.title}</h3>
+        <p>{joke.content}</p>
       </div>
     ))
    }
